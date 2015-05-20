e9c2eace65fceee1838d5b4acc4e5cd59b965805
Olivier Louwaars <o_louwaars@hotmail.com>
2015-05-20 15:43:03 +0200

    protocol

--- geo/alertmerger.py
+++ geo/alertmerger.py
@@ -1,15 +1,15 @@
 import pickle
 aDict= {}
 
-for i in range(6):
+for i in range(14):
 	print(i)
-	alertfile='alertlocs'+str(i)+'.pickle'
+	alertfile='geolocs'+str(i)+'.pickle'
 	alerts=pickle.load(open(alertfile, 'rb'))
 
 	for key in alerts.keys():
 		aDict[key]=alerts.get(key)
 
-with open('alertlocsall.pickle','wb') as f:
+with open('geolocsall.pickle','wb') as f:
 		pickle.dump(aDict,f)
 	
 
